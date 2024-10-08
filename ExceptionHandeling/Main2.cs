b8ac5cd8b176274c86b23290af5a9575e7257d84
HassanNewton <hassan.hussin@newton.se>
2024-10-08 16:41:25 +0200

    Add project files.

---
+++ ExceptionHandeling/Main2.cs
@@ -0,0 +1,36 @@
+﻿namespace ExceptionHandeling
+{
+    internal class Main2
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = null;
+
+            try
+            {
+                reader = new StreamReader("C:\\Users\\Ettan.malmo\\source\\repos\\ExceptionHandeling\\ExceptionHandeling\\TextFile1.txt");
+                string content = reader.ReadToEnd();
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Filen hittas ej, kolla pathen");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                if (reader != null)
+                {
+                    reader.Close();
+                }
+            }
+
+
+
+        }
+    }
+}
---
+++ ExceptionHandeling/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace ExceptionHandeling
+{
+    internal class Program
+    {
+        //static void Main(string[] args)
+        //{
+        //    try
+        //    {
+        //        Console.WriteLine("Ange ett tal");
+        //        int nbr1 = Convert.ToInt32(Console.ReadLine());
+
+        //        Console.WriteLine("Ange ett tal");
+        //        int nbr2 = Convert.ToInt32(Console.ReadLine());
+
+        //        int result = nbr1 / nbr2;
+
+        //        Console.WriteLine(result);
+        //    }
+        //    catch (DivideByZeroException ex)
+        //    {
+        //        Console.WriteLine("Du har försökt dividera med noll som inte är okej");
+        //        Console.WriteLine(ex.Message);
+
+        //    }
+        //    catch (FormatException ex)
+        //    {
+        //        Console.WriteLine("Mata in ett heltal och inget annat");
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //}
+    }
+}
