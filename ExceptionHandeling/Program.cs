b174d5cd1071d16d220dd40078b074b49ca058c5
HassanNewton <hassan.hussin@newton.se>
2024-10-08 16:42:16 +0200

    .

--- ExceptionHandeling/Program.cs
+++ ExceptionHandeling/Program.cs
@@ -2,35 +2,35 @@
 {
     internal class Program
     {
-        //static void Main(string[] args)
-        //{
-        //    try
-        //    {
-        //        Console.WriteLine("Ange ett tal");
-        //        int nbr1 = Convert.ToInt32(Console.ReadLine());
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Ange ett tal");
+                int nbr1 = Convert.ToInt32(Console.ReadLine());
 
-        //        Console.WriteLine("Ange ett tal");
-        //        int nbr2 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Ange ett tal");
+                int nbr2 = Convert.ToInt32(Console.ReadLine());
 
-        //        int result = nbr1 / nbr2;
+                int result = nbr1 / nbr2;
 
-        //        Console.WriteLine(result);
-        //    }
-        //    catch (DivideByZeroException ex)
-        //    {
-        //        Console.WriteLine("Du har försökt dividera med noll som inte är okej");
-        //        Console.WriteLine(ex.Message);
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Du har försökt dividera med noll som inte är okej");
+                Console.WriteLine(ex.Message);
 
-        //    }
-        //    catch (FormatException ex)
-        //    {
-        //        Console.WriteLine("Mata in ett heltal och inget annat");
-        //        Console.WriteLine(ex.Message);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        Console.WriteLine(ex.Message);
-        //    }
-        //}
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Mata in ett heltal och inget annat");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
